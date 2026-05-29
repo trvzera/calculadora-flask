f3c349ceb7a682a7e89f0e20b1a240a02c88bc16
trvzera <giovanni.cm.trivellato@gmail.com>
2026-05-28 22:59:18 -0300

    Formula de bahskararakra

--- calculadora.py
+++ calculadora.py
@@ -39,8 +39,21 @@ def calcular():
         elif operacao == "**":
             resultado = num1 ** num2
             etapas = f"{num1} ** {num2} = {resultado}"
-        elif operacao == "bah":
-            print("TODO")
+        else:
+            num3 = float(request.form["num3"])
+            delta = (num2 ** 2)-4*num1*num3
+            if delta < 0:
+                resultado = "Não possui raiz real"
+                etapas = f"{num2}^2 - 4{num1}{num3}  = {delta}"
+            elif delta == 0:
+                x = ((num2*-1)+delta)/(2*num1)
+                resultado = x
+                etapas = f"- {num2} +-  √{delta} / 2{num1} = {x}"
+            else:
+                x1 = ((num2*-1)+math.sqrt(delta))/(2*num1)
+                x2 = ((num2*-1)-math.sqrt(delta))/(2*num1)
+                resultado = f"X1 = {x1} e X2 = {x2}"
+                etapas = f"- {num2} +-  √{delta} / 2*{num1} = {x1} ou {x2}"
         
     return render_template(
                 "calculadora.html",
